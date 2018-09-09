82d2c7d8097033430ea5e57ab335ae25c8d02505
tung2389 <36613569+tung2389@users.noreply.github.com>
2018-09-09 16:54:40 +0700

    Update App-firstreactprogram.js

--- src/App-firstreactprogram.js
+++ src/App-firstreactprogram.js
@@ -4,8 +4,8 @@ import './App.css';
 const blue='#4700ff';
 const green = '#39D1B4';
 const yellow = '#FFD712';
-const source1="https://kotopaint.vn/uploads/banner-dai-ly-son-koto.jpg";
-const source2="https://scontent.fhan2-3.fna.fbcdn.net/v/t34.0-12/28536365_2065549437015907_1269407837_n.png?_nc_cat=0&oh=9a7efb40ee3f90db21f30a579bc5d0e9&oe=5AD1B25F";
+const source1="";
+const source2="https://reactjs.org/logo-og.png";
 const styles = {
   color:      'green',
   fontSize: 100
@@ -13,14 +13,33 @@ const styles = {
 class App_firstreactprogram extends Component {
   constructor(props) {
     super(props);
-	this.state = { mood: 'good',color: green,picture: source1,userInput: ''};
+	this.state = { mood: 'good',color: green,picture: source1,userInput: '',item : [],target:"",number:0};
 	this.handleUserInput= this.handleUserInput.bind(this);
     this.changeColor=this.changeColor.bind(this);
     this.toggleMood = this.toggleMood.bind(this);
     this.changeSource=this.changeSource.bind(this);
     this.display=this.display.bind(this);
     this.returnDiv=this.returnDiv.bind(this);
-  }
+    this.GetData=this.GetData.bind(this);
+    this.check=this.check.bind(this);
+    this.Search=this.Search.bind(this);
+    this.increase=this.increase.bind(this);
+}
+Search(e)
+{
+  this.setState({target:e.target.value});
+}
+GetData()
+{
+  fetch("https://swapi.co/api/people/?format=json")
+    .then(response => response.json())
+    .then( ({results:items}) => {this.setState({item:items})})
+}
+check()
+{
+  this.GetData();
+  console.log(this.state.item);
+}
     handleUserInput(e)
   {
     this.setState({userInput : e.target.value});
@@ -50,14 +69,28 @@ class App_firstreactprogram extends Component {
  {
    alert(this.state.userInput);
  }
+ increase()
+ {
+   this.setState({number:this.state.number+1});
+ }
  returnDiv()
  {
    return(
     <div style={styles}> This is my first react program </div>
    )
  }
+ /*<button onClick={this.GetData}>GetData</button>
+      {items.map(item => <h4 key = {item.name}>{item.name}</h4>)}*/
   render() {
     var returnDiv=this.returnDiv();
+    let array = this.state.item;
+    if(this.state.target!=="")
+    {
+      array = array.filter(element => element.name.toLowerCase().includes(this.state.target.toLowerCase()))
+    }
+    const s = {
+      fontSize : 30,
+    }
     return (
     <div>
 	 <div align="center">
@@ -66,6 +99,15 @@ class App_firstreactprogram extends Component {
         <input type="text" onChange={this.handleUserInput} value={this.state.userInput} />
         <button onClick={this.display}>click to display</button>
       </div>
+      <button onClick={this.check}>GetData</button>
+    <div>
+    <h1> Move your mouse on the number to see the change </h1>
+    <label style = {s} onMouseMove={this.increase}>{this.state.number}</label>
+    <br/>
+    <h1> Search box </h1>
+    <input onChange = {this.Search}/>
+    {array.map(element => <h4 key = {element.name}>{element.name}</h4>)}
+    </div>
       <div style={{background: this.state.color}}>
         <h1>
           Change my color
@@ -83,12 +125,12 @@ class App_firstreactprogram extends Component {
         <button onClick={this.say}>
          :)
           </button>
-        <h1> du lich </h1>
+        <h1> Click to display image </h1>
         <button onClick={this.changeSource}>
-        dulich
+        Image
         </button>
-        <h1> this is du lich </h1>
-        <img src={this.state.picture} width="600px" height="600px"/>
+        <br/>
+        <img src={this.state.picture}/>
       </div>
       </div>
     );
